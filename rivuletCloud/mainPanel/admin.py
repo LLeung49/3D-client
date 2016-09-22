c84f2c8a42bc5ca69a6debd542d5ed99608fd0ab
Lucien <328666566@qq.com>
2016-09-22 16:16:23 +1000

    unbuntu

--- rivuletCloud/mainPanel/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import HistoryTask
-
-admin.site.register(HistoryTask)
--- rivuletCloud/mainPanel/task_forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import HistoryTask
-
-
-class HistoryTaskForm(forms.Form):
-    user = forms.CharField(max_length=50)
-    desc = forms.CharField(max_length=1000)
-    file = forms.FileField()
